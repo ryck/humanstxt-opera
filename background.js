edc2d8c2997ece0ab8070da378880afd570724d6
Ricardo Gonzalez <rickgc@gmail.com>
2011-04-12 17:32:20 +0100

    bg script

--- background.js
+++ background.js
@@ -1,27 +1,27 @@
-		window.addEventListener( 'load', function(){
-			var theButton;
-			var ToolbarUIItemProperties = {
-					disabled: false,
-					title: 'humanstxt',
-					icon: 'icons/icon-18.png',
-					popup: {
-						href: 'popup.html',
-						width: 200,
-						height: 300
-					}
-				}
-			theButton = opera.contexts.toolbar.createItem(ToolbarUIItemProperties);
+window.addEventListener( 'load', function(){
+	var theButton;
+	var ToolbarUIItemProperties = {
+			disabled: false,
+			title: 'humanstxt',
+			icon: 'icons/icon-18.png',
+			popup: {
+				href: 'popup.html',
+				width: 200,
+				height: 300
+			}
+		}
+	theButton = opera.contexts.toolbar.createItem(ToolbarUIItemProperties);
+  
 
-    	var tab = opera.extension.tabs.getFocused();
-    	var uri = tab.url;
-
-      
+    // Listen for messages from the UserJS.
+    opera.extension.onmessage = function(event){
+     	window.opera.postError('URL: ' + event.data);
+      uri = event.data; 
       htxt = null;
-
-			var req = new XMLHttpRequest;
-			
-			req.onreadystatechange = function () {
-				if (req.readyState == 4 && req.status == 200) {
+	    var req = new XMLHttpRequest;
+	
+	    req.onreadystatechange = function () {
+		    if (req.readyState == 4 && req.status == 200) {
 
           htxt = req.responseText;
           rps = req.getResponseHeader("Content-Type");
@@ -33,11 +33,11 @@
             opera.contexts.toolbar.removeItem(theButton);
           }
 
-				}
-			
-			req.open('GET', uri + "/humans.txt", true);
-			req.send();
-						
-			
-			
-		}, false );
+		    }
+	
+	    req.open('GET', uri + "/humans.txt", true);
+	    req.send();
+
+	}	
+	
+}, false );
---
+++ includes/base.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @include http://*
+// ==/UserScript==
+
+// Background scripts can't directly interact with web pages.
+// To solve this, we create this UserJS to send messages to the background script.
+window.addEventListener('load', function(){
+	window.opera.postError('Sending message to background.js');
+	opera.extension.postMessage(document.url);
+}, false);
