552249b7d81c758a6862bcafdc3e0ad3f13ffdc9
Shwetank Dixit <emailshwetank@gmail.com>
2011-04-16 03:27:36 -0700

    changed document.url to document.URL

--- includes/base.js
+++ includes/base.js
@@ -6,5 +6,5 @@
 // To solve this, we create this UserJS to send messages to the background script.
 window.addEventListener('load', function(){
 	window.opera.postError('Sending message to background.js');
-	opera.extension.postMessage(document.url);
+	opera.extension.postMessage(document.URL);
 }, false);
